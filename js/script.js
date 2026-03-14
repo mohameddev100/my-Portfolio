9af4b5328b677bd3607df1fa49ed4451177ad82e
mohamedell <mo@gmail.com>
2026-03-14 18:14:27 +0100

    Modify some responsive

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-let countries = document.getElementById("country");
+const countries = document.getElementById("country");
+const list_btn = document.getElementById("list-links");
+const nav_link = document.getElementById("link");
+
+console.log(nav_link);
+
+list_btn.addEventListener("click", function () {
+    nav_link.classList.toggle("showHide");
+});
 
 async function getCountry() {
-    let data = await fetch("../country-flag.json");
-    let response = await data.json();
+    try {
+        let data = await fetch("../country-flag.json");
+        let response = await data.json();
 
-    response.forEach((country) => {
-        let flag = country.flag;
-        let myCountry = country.country;
+        response.forEach((country) => {
+            let flag = country.flag;
+            let myCountry = country.country;
 
-        let option = document.createElement("option");
-        option.classList.add("option-item");
-        option.innerHTML = `
-                <span>${myCountry}</span>
+            let option = document.createElement("option");
+            option.classList.add("option-item");
+            option.textContent = `
+                ${myCountry}
         `;
-        countries.appendChild(option);
-    });
+            countries.appendChild(option);
+        });
+    } catch (error) {
+        console.log("Error al cargar los países:", error);
+    }
 }
 
 getCountry();
